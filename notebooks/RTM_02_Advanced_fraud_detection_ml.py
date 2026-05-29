c873408cf88ea28c29daa8a2eb7fe229e4bea349
harneet9 <harneet9@gmail.com>
2026-05-29 21:44:21 +0530

    Chnages in RTM_02

--- notebooks/RTM_02_Advanced_fraud_detection_ml.py
+++ notebooks/RTM_02_Advanced_fraud_detection_ml.py
@@ -341,6 +341,14 @@ transaction_schema = StructType([
     StructField("event_time", StringType()),
 ])
 
+# --- Azure Event Hubs SASL_SSL config ---
+eventhub_conn_string = dbutils.secrets.get(scope="kafka-scope", key="kafka-connection-string")
+kafka_sasl_jaas_config = (
+    'kafkashaded.org.apache.kafka.common.security.plain.PlainLoginModule required '
+    'username="$ConnectionString" '
+    f'password="{eventhub_conn_string}";'
+)
+
 raw_stream = (
     spark.readStream
     .format("kafka")
@@ -348,7 +356,11 @@ raw_stream = (
     .option("assign", json.dumps({input_topic: [0, 1]}))  # Read only 2 of 8 partitions
     .option("startingOffsets", "latest")
     .option("failOnDataLoss", "false")
-    .option("kafka.security.protocol", "SSL")
+    .option("kafka.security.protocol", "SASL_SSL")
+    .option("kafka.sasl.mechanism", "PLAIN")
+    .option("kafka.sasl.jaas.config", kafka_sasl_jaas_config)
+    .option("kafka.request.timeout.ms", "60000")
+    .option("kafka.session.timeout.ms", "30000")
     .load()
 )
 
@@ -815,6 +827,13 @@ print(f"Loaded model as Spark UDF: {scoring_model_uri}")
 # COMMAND ----------
 
 # Read from the same Kafka topic (2 partitions to stay within slot limits)
+# --- Azure Event Hubs SASL_SSL config (same as Section 4.1) ---
+scoring_sasl_jaas_config = (
+    'kafkashaded.org.apache.kafka.common.security.plain.PlainLoginModule required '
+    'username="$ConnectionString" '
+    f'password="{eventhub_conn_string}";'
+)
+
 raw_stream_scoring = (
     spark.readStream
     .format("kafka")
@@ -822,7 +841,11 @@ raw_stream_scoring = (
     .option("assign", json.dumps({input_topic: [0, 1]}))
     .option("startingOffsets", "latest")
     .option("failOnDataLoss", "false")
-    .option("kafka.security.protocol", "SSL")
+    .option("kafka.security.protocol", "SASL_SSL")
+    .option("kafka.sasl.mechanism", "PLAIN")
+    .option("kafka.sasl.jaas.config", scoring_sasl_jaas_config)
+    .option("kafka.request.timeout.ms", "60000")
+    .option("kafka.session.timeout.ms", "30000")
     .load()
 )
 
